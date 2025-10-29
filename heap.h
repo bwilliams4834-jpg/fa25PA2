0b1cc8dd2c1f071d875180193134586f98421559
bwilliams4834-jpg <bwilliams4834@sdsu.edu>
2025-10-28 21:40:51 -0700

    finsihed most of heap.h

--- heap.h
+++ heap.h
@@ -16,20 +16,70 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        if (size == 0) {
+            weightArr[0] = idx;
+            size++;
+            return;
+        }
+        if (!weightArr[2*size + 1]) {
+            weightArr[2*size + 1] = idx;
+            size++;
+            return;
+        }
+        if (!weightArr[2*size + 2]) {
+            weightArr[2*size + 2] = idx;
+            size++;
+        }
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) {
+            cout << "Empty Heap.\n" << endl;
+            return -1;
+        }
+        return -1; //placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        if (size == 0) {
+            cout << "Empty Heap.\n" << endl;
+            return;
+        }
+        while (pos > 0) {
+            if (weightArr[pos] < weightArr[(pos - 1)/2]) {
+                int tmp = weightArr[pos];
+                weightArr[pos] = weightArr[(pos - 1)/2];
+                weightArr[(pos - 1)/2] = tmp;
+            }
+            //recursive call with the parent node.
+            upheap((pos - 1)/2, weightArr);
+        }
+
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        if (size == 0) {
+            cout << "Empty Heap.\n" << endl;
+            return;
+        }
+        while (pos < size) {
+            if (weightArr[pos] > weightArr[2*pos + 1] && weightArr[pos] > weightArr[2*pos + 2]) {
+                int tmp = weightArr[pos];
+                weightArr[pos] = weightArr[2*pos + 1];
+                weightArr[2*pos + 1] = tmp;
+
+            }
+            if (weightArr[pos] > weightArr[2*pos + 2]) {
+                int tmp = weightArr[pos];
+                weightArr[pos] = weightArr[2*pos + 2];
+                weightArr[2*pos + 2] = tmp;
+
+            }
+        }
     }
 };
 
